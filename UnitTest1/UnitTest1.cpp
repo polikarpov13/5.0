36a348d5655f7d45e6ec08cb073940adb0a72d81
polikarpov13 <vladyslav.polikarpov.ik.2021@lpnu.ua>
2021-10-20 13:40:19 +0300

    second test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.1/5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = g(1, 2);
+			Assert::AreEqual(t, 7.0);
+		}
+	};
+}
